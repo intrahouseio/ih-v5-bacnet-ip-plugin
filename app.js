7d5fa8f871748715460ae2e866a39b50c498136b
goodspeed1986 <goodspeed1986@gmail.com>
2022-07-22 18:33:27 +0300

    update read, subscribe

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Bacnet = require('node-bacnet');
 
 const Scanner = require("./lib/scanner");
 const client = require("client");
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 module.exports = async function (plugin) {
   const bacnetClient = new Bacnet({apduTimeout: plugin.params.data.apduTimeout});
@@ -21,9 +22,22 @@ module.exports = async function (plugin) {
     plugin.log('ACT data=' + util.inspect(data));
     write(data);
   });
-  plugin.channels.onChange(async function () {
-    const channels = await plugin.channels.get();
-    monitor(channels);
+  plugin.channels.onChange(async function (data) {
+    plugin.log('Change' + util.inspect(data));
+    for (i =0; i<data.length; i++) {
+      if (data[i].op == 'delete') {
+        let result = await client.unsubscribe(data[i].address, JSON.parse(data[i].chan).objectId);
+        plugin.log('UnsubscribeCOV' + result, 2);
+        delete channelsValue[data[i].chan];
+        await sleep(200);
+      }
+      if (data[i].op == 'add') {
+        let result = await client.subscribe(data[i].address, JSON.parse(data[i].chan).objectId);
+        plugin.log('SubscribeCOV' + result, 2);
+        channelsValue[data[i].id] = null;
+        await sleep(200);
+      }    
+    }
   });
 
   bacnetClient.on('error', (err) => {
@@ -34,23 +48,27 @@ module.exports = async function (plugin) {
   async function monitor(channels) {
       for (i =0; i<channels.length; i++) {
         if (channels[i].r) {
-          let result = await client.unsubscribe(channels[i].address, JSON.parse(channels[i].chan).objectId);
-        plugin.log('UnsubscribeCOV' + result, 2);
         result = await client.subscribe(channels[i].address, JSON.parse(channels[i].chan).objectId);
         plugin.log('SubscribeCOV' + result, 2);
+        await sleep(200);
         }    
       }
-      
-      bacnetClient.on('covNotifyUnconfirmed', (data) => {
+
+      bacnetClient.on('covNotifyUnconfirmed', async (data) => {
         //plugin.log('Received COV: ' + JSON.stringify(data, null, 2));
         let id = {};
         id.objectId = data.payload.monitoredObjectId; 
-        id.address = data.header.sender.address;  
+        id.address = data.header.sender.address;
+
+        if (channelsValue[JSON.stringify(id)] === undefined) {
+          let result = await client.unsubscribe(id.address, id.objectId);
+          plugin.log('UnsubscribeCOV' + result, 2);
+        }
         if (channelsValue[JSON.stringify(id)] != data.payload.values[0].value[0].value) {
           channelsValue[JSON.stringify(id)] = data.payload.values[0].value[0].value;
-
           plugin.sendData([{ id: JSON.stringify(id), value: data.payload.values[0].value[0].value}]);
         } 
+        
       });
   }
 
@@ -64,11 +82,20 @@ module.exports = async function (plugin) {
     plugin.log('Write ERROR: ' + util.inspect(e));
     }
   }
-  function main() {
-    monitor(plugin.channels.data);
+  async function main(channels) {  
+    for (i =0; i<channels.length; i++) {
+      if (channels[i].r) {
+      const value =  await client.readProperty(channels[i].address, JSON.parse(channels[i].chan).objectId);
+      channelsValue[channels[i].id] = value.values[0].value;
+      await sleep(100);
+      }    
+    }
+    const arr = Object.keys(channelsValue).map(i => ({id:i, value:channelsValue[i]}));
+    plugin.sendData(arr);
+    monitor(channels);
   }
 
-  main();
+  main(plugin.channels.data);
 
   // --- События плагина ---
   // Сканирование
